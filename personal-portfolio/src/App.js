96f60e2f058eaa7e29cd564dca75294b33e9dbb4
Pratyush Sinha <105473440+pratyushsinhaa@users.noreply.github.com>
2024-10-07 15:37:59 +0530

    picture and david bowie

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -13,9 +13,14 @@ function App() {
             <div className="bento-item project1">Project 1</div>
             <div className="bento-item project2">Project 2</div>
             <div className="bento-item image">
-              <img src="/path/to/your/image.jpg" alt="My Image" />
+              <img src="mee.jpeg" alt="My Image" />
             </div>
-            <div className="bento-item playing">Currently Playing</div>
+            <div className="bento-item playing">
+              <img src="spaceoddity.jpeg" alt="Currently Playing" className="playing-image" />
+              <a href="https://music.apple.com/us/album/space-oddity/181456799?i=181457000" target="_blank" rel="noopener noreferrer" className="playing-link">
+                Currently Playing
+              </a>
+            </div>
           </div>
         </header>
         <Routes>
